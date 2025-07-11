be4045e3931a59113f3666568fd21bf8ae31e0ba
Empereur010 <saidzoungrana04@gmail.com>
2025-07-11 00:41:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-/**
- * @author NTKhang
- * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
- * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
- * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
- *
- * English:
- * ! Please do not change the below code, it is very important for the project.
- * It is my motivation to maintain and develop the project for free.
- * ! If you change it, you will be banned forever
- * Thank you for using
- *
- * Vietnamese:
- * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án.
- * Nó là động lực để tôi duy trì và phát triển dự án miễn phí.
- * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
- * Cảm ơn bạn đã sử dụng
- */
-
+const express = require("express");
 const { spawn } = require("child_process");
 const log = require("./logger/log.js");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Démarrer GoatBot
 function startProject() {
 	const child = spawn("node", ["Goat.js"], {
 		cwd: __dirname,
@@ -28,11 +14,20 @@ function startProject() {
 	});
 
 	child.on("close", (code) => {
-		if (code == 2) {
-			log.info("Restarting Project...");
+		if (code === 2) {
+			log.info("🔁 Redémarrage automatique de GoatBot...");
 			startProject();
 		}
 	});
 }
 
 startProject();
+
+// Serveur web obligatoire pour Render
+app.get("/", (req, res) => {
+	res.send("✅ GoatBot est en ligne sur Render");
+});
+
+app.listen(PORT, () => {
+	console.log(`✅ Serveur Express lancé sur le port ${PORT}`);
+});
