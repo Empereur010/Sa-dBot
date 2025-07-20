d05e7ec9e61f65fea6a2279a65c79943891035e4
Empereur010 <saidzoungrana04@gmail.com>
2025-07-20 02:49:48 +0000

    Update and rename uptime2.js to uptime.js

---
+++ scripts/cmds/uptime.js
@@ -0,0 +1,28 @@
+ module.exports = {
+  config: {
+    name: "uptime",
+    aliases: ["up", "upt"],
+    version: "1.0",
+    author: "VEX_ADNAN",
+    role: 0,
+    shortDescription: {
+      en: "Displays the uptime of the bot."
+    },
+    longDescription: {
+      en: "Displays the amount of time that the bot has been running for."
+    },
+    category: "System",
+    guide: {
+      en: "Use {p}uptime to display the uptime of the bot."
+    }
+  },
+  onStart: async function ({ api, event, args }) {
+    const uptime = process.uptime();
+    const seconds = Math.floor(uptime % 60);
+    const minutes = Math.floor((uptime / 60) % 60);
+    const hours = Math.floor((uptime / (60 * 60)) % 24);
+    const days = Math.floor(uptime / (60 * 60 * 24));
+    const uptimeString = `\n\n𖤍 ${hours} 𝐡𝐞𝐮𝐫𝐞𝐬\n\n𖤍 ${minutes} 𝐦𝐢𝐧𝐮𝐭𝐞𝐬ᴇs\n\n𖤍 ${seconds} 𝐬𝐞𝐜𝐨𝐧𝐝𝐞ᴅ\n\n✧────────────────✧`;
+    api.sendMessage(`✧────────────────✧\n\n💥 𝐁𝐨𝐧𝐣𝐨𝐮𝐫 𝐦𝐚𝐢̂𝐭𝐫𝐞 , 𝐣𝐞 𝐟𝐨𝐧𝐜𝐭𝐢𝐨𝐧𝐧𝐞 𝐭𝐨𝐮𝐣𝐨𝐮𝐫𝐬𝐫 𝐝𝐞𝐩𝐮𝐢𝐬 ↓\n↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ ${uptimeString}`, event.threadID);
+  }
+};
